45d1584993d6dca51864836b23c616925c31b605
mkiken <mori9@is.titech.ac.jp>
2013-10-23 01:44:00 +0900

    add several languages.

--- cpp/template.cpp
+++ cpp/template.cpp
@@ -45,6 +45,7 @@ const double EPS = 1e-10;
 //const int dy[] = {0, 0, 1, -1, -1, -1, 1, 1};
 //const int weight[] = {0,1,10,100,1000,10000,100000,1000000,10000000};
 //priority_queue< int, vector<int>, greater<int> > q;
+#define MAX_N 1000
 
 void doIt(){
 }
