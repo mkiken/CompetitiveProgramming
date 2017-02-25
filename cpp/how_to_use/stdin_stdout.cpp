e3cafc098ae6e4bb3984091aa6ee67ea1bc686e5
mkiken <mkiken0@gmail.com>
2017-02-26 05:46:09 +0900

    標準入力/出力

---
+++ cpp/how_to_use/stdin_stdout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// https://gist.github.com/rigibun/7905920
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int x;
+  cin >> x;
+  cout << x * x * x << "\n";
+}
