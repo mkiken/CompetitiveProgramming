cc3a9ca7cd61badbc2afa66ce6723bd6592c7c90
mkiken <mkiken0@gmail.com>
2018-02-18 21:29:25 +0900

    [cpp] BITのsum版改良

--- cpp/data_structure/BIT/bit_sum.cpp
+++ cpp/data_structure/BIT/bit_sum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <cassert>
 #include <cmath>
 #include <vector>
 #include <algorithm>
@@ -49,119 +50,72 @@ const double EPS = 1e-10;
 //const int dy[] = {0, 0, 1, -1, -1, -1, 1, 1};
 //const int weight[] = {0,1,10,100,1000,10000,100000,1000000,10000000};
 //priority_queue< int, vector<int>, greater<int> > q;
+
 /*
-  Binary Indexed Tree
+  sum Binary Indexed Tree
+*/
+class BitSum{
+protected:
+  static const int max_value = 100000 + 2;
+  ll bit[max_value + 1];
 
-  void add(int idx, ll val, ll* ary)
-  BIT aryのidxにvalを追加する．
-  idxは0-indexed.idx = -1で0を返す(ary[0]にアクセス)．
-
-  ll sum(int idx, ll *ary)
-  idxまでの総和を求める．
-  idx = -1で0を返す（ary[0]までの総和）．
-
-  void change(int idx, ll val, ll* ary)
-  idx番目をvalに変更する．
-
- */
-
-
-#define MAX_N 100000 + 2
-ll bit[MAX_N + 1];
-void add(int idx, ll val, ll* ary){
-  int p = idx+1;
-  while(p < MAX_N){
-    ary[p] += val;
-    p += p & -p;
+public:
+  BitSum(int size) {
+    assert(size < max_value);
+    init();
   }
-}
-ll sum(int idx, ll *ary){
-  ll res = 0;
-  int p = idx+1;
-  while(0 < p){
-    res += ary[p];
-    p -= p & -p;
+  void init(){
+    for (int i = 0; i < max_value; i++){
+      bit[i] = 0;
+    }
   }
-  return res;
-}
-void change(int idx, ll val, ll* ary){
-  ll s = sum(idx, ary) - sum(idx-1, ary);
-  add(idx, val - s, ary);
-}
-
-ll bit1[MAX_N];
-void badd(int l, int r, ll val){
-  add(l, -val * (l - 1), bit);
-  add(l, val, bit1);
-  add(r + 1, r * val, bit);
-  add(r + 1, -val, bit1);
-}
-ll ssum(int idx){
-  return sum(idx, bit1) * idx + sum(idx, bit);
-}
-
-//転倒数を求める（蟻本）
-// void doIt(){
-// 	int t, n, a[MAX_N];
-// 	ll ans = 0;
-// 	cin >> t;
-// 	while(t--){
-// 		cin >> n;
-// 		Rep(i, n) cin >> a[i];
-// 		ans = 0;
-// 		fill(bit0, bit0 + n + 1, 0);
-// 		Rep(i, n){
-// 			ans += i - sum(a[i], bit0);
-// 			add(a[i], 1, bit0);
-// 		}
-// 		cout << ans << endl;
-// 	}
-// }
-
-//http://www.codechef.com/ODCD2013/problems/LETHOR
-// void doIt(){
-// 	int t, n, a[MAX_N], b[MAX_N];
-// 	ll ans = 0;
-// 	vector<P> v;
-// 	cin >> t;
-// 	while(t--){
-// 		cin >> n;
-// 		v.clear();
-// 		Rep(i, n) cin >> b[i], v.pb(P(b[i], i));
-// 		vsort(v);
-// 		Rep(i, n){
-// 			a[v[i].second] = i+1;
-// 		}
-// 		ans = 0;
-// 		fill(bit0, bit0 + n + 1, 0);
-// 		Rep(i, n){
-// 			ans += i - sum(a[i], bit0);
-// 			add(a[i], 1, bit0);
-// 		}
-// 		cout << ans << endl;
-// 	}
-// }
+  void add(int idx, ll val){
+    int p = idx+1;
+    while(p < max_value){
+      bit[p] += val;
+      p += p & -p;
+    }
+  }
+  /**
+   * [0, idx]のsumを求める
+   */
+  ll calc(int idx){
+    ll res = 0;
+    int p = idx+1;
+    while(0 < p){
+      res += bit[p];
+      p -= p & -p;
+    }
+    return res;
+  }
+  /**
+   * [idx1, idx2]のsumを求める
+   */
+  ll range(int idx1, int idx2) {
+    return calc(idx2) - calc(idx1 - 1);
+  }
+};
 
 void doIt(){
-  add(0, 2, bit);
-  add(1, 5, bit);
-  add(2, 15, bit);
-  cout << sum(0, bit) << endl;
-  cout << sum(1, bit) << endl;
-  cout << sum(2, bit) << endl;
-  change(0, 27, bit);
-  cout << sum(0, bit) << endl;
-  cout << sum(1, bit) << endl;
-  cout << sum(2, bit) << endl;
-}
+  BitSum bs = BitSum(100);
+  bs.add(1, 10);
+  bs.add(2, 20);
+  bs.add(3, 30);
 
-void doIt2(){
-  badd(0, 5, 10);
-  badd(0, 1, 100);
-  cout << ssum(0) << endl;
-  cout << ssum(4) << endl;
-  cout << ssum(5) << endl;
-  cout << ssum(0) << endl;
+  assert(bs.calc(0) == 0);
+  assert(bs.calc(1) == (10));
+  assert(bs.calc(2) == (10 + 20));
+  assert(bs.calc(3) == (10 + 20 + 30));
+
+  assert(bs.range(0, 0) == 0);
+  assert(bs.range(0, 1) == 10);
+  assert(bs.range(0, 3) == (10 + 20 + 30));
+  assert(bs.range(1, 1) == 10);
+  assert(bs.range(1, 2) == (10 + 20));
+  assert(bs.range(2, 3) == (20 + 30));
+  assert(bs.range(1, 3) == (10 + 20 + 30));
+
+  printf("test passed!\n");
 }
 
 int main() {
