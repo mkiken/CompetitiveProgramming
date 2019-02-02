67843c029ecb8ceb78ff2db84811ae3b8fab115b
mkiken <mkiken0@gmail.com>
2019-02-02 15:41:53 +0900

    [cpp] 行列計算

--- cpp/data_structure/matrix.cpp
+++ cpp/data_structure/matrix.cpp
@@ -1,94 +1,51 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
+#include <cassert>
 #include <climits>
-#include <sstream>
-#include <functional>
-#include <complex>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-#define len(array)  (sizeof (array) / sizeof *(array))
-#define rep(i, s, e) for(int i = (s);i < (e);i++)
-#define Rep(i, e) for(int i = 0;i < (e);i++)
-#define rrep(i, e, s) for(int i = (e);(s) <= i;i--)
-#define Rrep(i, e) for(int i = e;0 <= i;i--)
-#define mrep(i, e, t1, t2) for(map<t1, t2>::iterator i = e.begin(); i != e.end(); i++)
-#define vrange(v) v.begin(), v.end()
-#define vrrange(v) v.rbegin(), v.rend()
-#define vsort(v) sort(vrange(v))
-#define vrsort(v) sort(vrrange(v))
-#define arange(a) a, a + len(a)
-#define asort(a) sort(arange(a))
-#define arsort(a, t) sort(arange(a), greater<t>())
-#define afill(a, v) fill(arange(a), v)
-#define afill2(a, v, t) fill((t *)(a), (t *)((a) + len(a)), v)
-#define fmax(a, b) ((a) < (b)? (b) : (a))
-#define fmin(a, b) ((a) > (b)? (b) : (a))
-#define fabs(a) ((a) < 0? -(a) : (a))
-#define pb push_back
-#define fst first
-#define snd second
-#define rg(e, s, t) s <= e && e < t
-#define PQDecl(name, tp) priority_queue< tp, vector<tp>, greater<tp> > name
-#define dq(q) q.top();q.pop();
-#define sz(v) ((int)(v).size())
-//#define X real()
-//#define Y imag()
-//typedef unsigned int ui;
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<int, int> P;
-typedef pair<ll, ll> PL;
-//typedef complex<double> p;
-const int INF = (int)2e9;
-const int MOD = (int)1e9 + 7;
-const double EPS = 1e-10;
-//const int dx[] = {1, -1, 0, 0, 1, -1, -1, 1};
-//const int dy[] = {0, 0, 1, -1, -1, -1, 1, 1};
-//const ll weight[] = {1e0,1e1,1e2,1e3,1e4,1e5,1e6,1e7,1e8,1e9,1e10,1e11,1e12,1e13};
-typedef struct _Datum {
-  int fst,snd,trd;
-  _Datum(int arg1 = 0, int arg2 = 0 , int arg3 = 0) {
-	fst = arg1; snd = arg2; trd = arg3;
-  }
-  bool operator <(const struct _Datum &e) const{
-    return fst == e.fst? (snd == e.snd? trd < e.trd : snd < e.snd) : fst < e.fst;
-  }
-  bool operator >(const struct _Datum &e) const{
-    return fst == e.fst? (snd == e.snd? trd > e.trd : snd > e.snd) : fst > e.fst;
-  }
-}datum;
+typedef long double ld;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef pair<int, int> ipair;
+typedef tuple<int, int, int> ituple;
 
+// const int INF = INT_MAX;
+// const ll INF = LLONG_MAX;
+// const int MOD = ((int)1e9 + 7);
+// const ld EPS = (1e-10);
+#define PI acosl(-1)
+#define MAX_N (50 + 2)
 
-#define MAX_N 100005
-
-typedef vector<int> vec;
-typedef vector<vec> matrix;
+typedef vector<VL> matrix;
 
 //行列A * B % m
 matrix mulMod(matrix &A, matrix &B, int m){
-  matrix C(sz(A), vec(sz(B[0])));
-  Rep(i, sz(A))
-    Rep(k, sz(B))
-      Rep(j, sz(B[0]))
-        C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % m;
+  matrix C(A.size(), VL(B[0].size()));
+  for (int i = 0; i < A.size(); i++){
+    for (int k = 0; k < B.size(); k++){
+      for (int j = 0; j < B[0].size(); j++){
+        C[i][j] = (C[i][j] + (A[i][k] * B[k][j] % m)) % m;
+      }
+    }
+  }
   return C;
 }
 
 //A^n
 matrix powMod(matrix A, ll n, int m){
-  matrix B(sz(A), vec(sz(A)));
-  Rep(i, sz(A)) B[i][i] = 1;
+  matrix B(A.size(), VL(A.size()));
+  for (int i = 0; i < A.size(); i++){
+    B[i][i] = 1;
+  }
   while(n > 0){
     if(n & 1) B = mulMod(B, A, m);
     A = mulMod(A, A, m);
